1bf9b6c26f51c0164f5c660f6fe0364f70247824
jewlys <jewlys@hotmail.com>
2022-12-12 13:23:48 +0100

    hasta donde joan ha hecho

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://kqobbpdyaurqorxemcog.supabase.co'
+const supabaseUrl = process.env.SUPABASE_URL
 const supabaseKey = process.env.SUPABASE_KEY
 export const supabase = createClient(supabaseUrl, supabaseKey);
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,5 +1,15 @@
 <template>
     Dashboard
+    <button @click = "login">Login!</button>
 </template>
 <script>
+import { mapStore } from 'pinia'
+import userStore from '../stores/user'
+export default{
+    methods: {
+        login() {
+
+        }
+    }
+}
 </script>
