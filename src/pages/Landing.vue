084bef58613d959a5000a2d958aaf2fc89b8a395
Lidia Ros <lidiarosmontes@gmail.com>
2022-12-14 16:20:04 +0100

    Pop Up commit

--- src/pages/Landing.vue
+++ src/pages/Landing.vue
@@ -65,7 +65,7 @@ drift.load("i3sks436p5vp");
               class="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-4xl lg:text-5xl xl:text-6xl"
             >
               <span class="block xl:inline">Tired of Notion's flaws </span>
-              <span class="block text-indigo-600 xl:inline"
+              <span class="block text-indigo-500 xl:inline"
                 >and ready to switch?</span
               >
             </h1>
@@ -77,9 +77,9 @@ drift.load("i3sks436p5vp");
             <div class="relative flex flex-col sm:flex-row sm:space-x-4">
               <a
                 href="/signup"
-                class="flex items-center w-full px-6 py-3 mb-3 text-lg text-white bg-indigo-600 rounded-md sm:mb-0 hover:bg-indigo-700 sm:w-auto"
+                class="flex items-center w-full px-6 py-3 mb-3 text-lg text-slate-900 bg-indigo-300 rounded-md sm:mb-0 hover:bg-indigo-700 sm:w-auto"
               >
-                Try It Free
+                Sign Up
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   class="w-5 h-5 ml-1"
@@ -160,7 +160,7 @@ drift.load("i3sks436p5vp");
             class="box-border relative py-1 pl-0 text-left text-gray-500 border-solid"
           >
             <span
-              class="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-yellow-300 rounded-full"
+              class="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-indigo-400 rounded-full"
               ><span class="text-sm font-bold">✓</span></span
             >
             Maximize productivity and growth
@@ -169,7 +169,7 @@ drift.load("i3sks436p5vp");
             class="box-border relative py-1 pl-0 text-left text-gray-500 border-solid"
           >
             <span
-              class="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-yellow-300 rounded-full"
+              class="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-indigo-400 rounded-full"
               ><span class="text-sm font-bold">✓</span></span
             >
             Speed past your competition
@@ -178,7 +178,7 @@ drift.load("i3sks436p5vp");
             class="box-border relative py-1 pl-0 text-left text-gray-500 border-solid"
           >
             <span
-              class="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-yellow-300 rounded-full"
+              class="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-indigo-400 rounded-full"
               ><span class="text-sm font-bold">✓</span></span
             >
             Learn the top techniques
@@ -210,7 +210,7 @@ drift.load("i3sks436p5vp");
             class="box-border relative py-1 pl-0 text-left text-gray-500 border-solid"
           >
             <span
-              class="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-yellow-300 rounded-full"
+              class="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-indigo-400 rounded-full"
               ><span class="text-sm font-bold">✓</span></span
             >
             Automated task management workflow
@@ -219,7 +219,7 @@ drift.load("i3sks436p5vp");
             class="box-border relative py-1 pl-0 text-left text-gray-500 border-solid"
           >
             <span
-              class="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-yellow-300 rounded-full"
+              class="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-indigo-400 rounded-full"
               ><span class="text-sm font-bold">✓</span></span
             >
             Detailed analytics for your data
@@ -228,7 +228,7 @@ drift.load("i3sks436p5vp");
             class="box-border relative py-1 pl-0 text-left text-gray-500 border-solid"
           >
             <span
-              class="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-yellow-300 rounded-full"
+              class="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-indigo-400 rounded-full"
               ><span class="text-sm font-bold">✓</span></span
             >
             Some awesome integrations
@@ -250,100 +250,13 @@ drift.load("i3sks436p5vp");
   </section>
 
   <!-- Section 4 -->
-  <section class="py-20 bg-gray-50">
-    <div
-      class="container items-center max-w-6xl px-4 px-10 mx-auto sm:px-20 md:px-32 lg:px-16"
-    >
-      <div class="flex flex-wrap items-center -mx-3">
-        <div class="order-1 w-full px-3 lg:w-1/2 lg:order-0">
-          <div class="w-full lg:max-w-md">
-            <h2
-              class="mb-4 text-3xl font-bold leading-tight tracking-tight sm:text-4xl font-heading"
-            >
-              Jam-packed with all the tools you need to succeed!
-            </h2>
-            <p class="mb-4 font-medium tracking-tight text-gray-400 xl:mb-6">
-              It's never been easier to build a business of your own. Our tools
-              will help you with the following:
-            </p>
-            <ul>
-              <li class="flex items-center py-2 space-x-4 xl:py-3">
-                <svg
-                  class="w-8 h-8 text-pink-500"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z"
-                  ></path>
-                </svg>
-                <span class="font-medium text-gray-500"
-                  >Faster Processing and Delivery</span
-                >
-              </li>
-              <li class="flex items-center py-2 space-x-4 xl:py-3">
-                <svg
-                  class="w-8 h-8 text-yellow-500"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4"
-                  ></path>
-                </svg>
-                <span class="font-medium text-gray-500"
-                  >Out of the Box Tracking and Monitoring</span
-                >
-              </li>
-              <li class="flex items-center py-2 space-x-4 xl:py-3">
-                <svg
-                  class="w-8 h-8 text-green-500"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"
-                  ></path>
-                </svg>
-                <span class="font-medium text-gray-500"
-                  >100% Protection and Security for Your App</span
-                >
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div class="w-full px-3 mb-12 lg:w-1/2 order-0 lg:order-1 lg:mb-0">
-          <img
-            class="mx-auto sm:max-w-sm lg:max-w-full"
-            src="https://cdn.devdojo.com/images/november2020/feature-graphic.png"
-            alt="feature image"
-          />
-        </div>
-      </div>
-    </div>
-  </section>
 
   <!-- Section 5 -->
   <section class="flex items-center justify-center py-20 bg-white min-w-screen">
     <div class="px-16 bg-white">
       <div class="container flex flex-col items-start mx-auto lg:items-center">
         <p
-          class="relative flex items-start justify-start w-full text-lg font-bold tracking-wider text-purple-500 uppercase lg:justify-center lg:items-center"
+          class="relative flex items-start justify-start w-full text-lg font-bold tracking-wider text-indigo-500 uppercase lg:justify-center lg:items-center"
         >
           Don't just take our word for it
         </p>
