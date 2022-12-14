8d86131bbeec60737616ee1750fca38018156b68
jewlys <jewlys@hotmail.com>
2022-12-14 13:13:00 +0100

    import newtask in dashboard

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -9,28 +9,14 @@
     <div class="flex flex-grow px-10 mt-4 space-x-6 overflow-auto">
       <div class="flex flex-col flex-shrink-0 w-72">
         <div class="flex items-center flex-shrink-0 h-10 px-2">
-          <span class="block text-sm font-semibold">Ready</span>
+          <span class="block text-sm font-semibold">To do</span>
           <span
             class="flex items-center justify-center w-5 h-5 ml-2 text-sm font-semibold text-indigo-500 bg-white rounded bg-opacity-30"
             >3</span
           >
-          <button
-            class="flex items-center justify-center w-6 h-6 ml-auto text-indigo-500 rounded hover:bg-indigo-500 hover:text-indigo-100"
-          >
-            <svg
-              class="w-5 h-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-              ></path>
-            </svg>
-          </button>
+        <div>
+          <newTask></newTask>
+        </div>
         </div>
         <div class="flex flex-col pb-2 overflow-auto">
           <div
@@ -52,35 +38,22 @@
               </svg>
             </button>
             <h4 class="mt-3 text-sm font-medium">
-              This is the title of the card for the thing that needs to be done.
+              This is the title of the card for the thing that it's being done.
             </h4>
           </div>
         </div>
       </div>
       <div class="flex flex-col flex-shrink-0 w-72">
         <div class="flex items-center flex-shrink-0 h-10 px-2">
-          <span class="block text-sm font-semibold">Review</span>
+          <span class="block text-sm font-semibold">Doing</span>
           <span
             class="flex items-center justify-center w-5 h-5 ml-2 text-sm font-semibold text-indigo-500 bg-white rounded bg-opacity-30"
             >3</span
           >
-          <button
-            class="flex items-center justify-center w-6 h-6 ml-auto text-indigo-500 rounded hover:bg-indigo-500 hover:text-indigo-100"
-          >
-            <svg
-              class="w-5 h-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-              ></path>
-            </svg>
-          </button>
+          <div>
+            <NewTask></NewTask>
+          </div>
+          
         </div>
         <div class="flex flex-col pb-2 overflow-auto">
           <div
@@ -115,23 +88,9 @@
             class="flex items-center justify-center w-5 h-5 ml-2 text-sm font-semibold text-indigo-500 bg-white rounded bg-opacity-30"
             >3</span
           >
-          <button
-            class="flex items-center justify-center w-6 h-6 ml-auto text-indigo-500 rounded hover:bg-indigo-500 hover:text-indigo-100"
-          >
-            <svg
-              class="w-5 h-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-              ></path>
-            </svg>
-          </button>
+         <div>
+          <newTask></newTask>
+         </div>
         </div>
         <div class="flex flex-col pb-2 overflow-auto">
           <div
@@ -165,5 +124,15 @@
 </template>
 
 <script>
-
+import NewTask from "../components/NewTask.vue"
+export default {
+  data() {
+    return {
+      newTask: NewTask
+  }
+},
+components: {
+  NewTask
+}
+};
 </script>
