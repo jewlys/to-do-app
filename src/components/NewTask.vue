0b31632de01b70a516aedb0a7dd14daf6c23fa3a
Benjamin Gelb <bengelb7@gmail.com>
2022-12-15 01:47:23 +0100

    final commit dec 14

--- src/components/NewTask.vue
+++ src/components/NewTask.vue
@@ -1,17 +1,17 @@
 <template>
-  <div>
-    <button @click="showform()"
+  <!-- <div> -->
+  <!-- <button @click="showform()"
       class="flex items-center justify-center w-6 h-6 ml-auto text-indigo-500 rounded hover:bg-indigo-500 hover:text-indigo-100"
       type="button" data-modal-toggle="staticModal">
       <svg class="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
       </svg>
-    </button>
+    </button> -->
 
 
 
-    <!-- form for adding the task here-->
-
+  <!-- form for adding the task here-->
+  <!-- 
     <div class="" v-if="mostrarform" id="defaultModal">
       <form @submit="addnewTask()">
 
@@ -34,22 +34,88 @@
       </form>
 
     </div>
-  </div>
+  </div> -->
 
+  <!-- other try with another form-->
 
+  <form>
+    <button type="button" v-on:click="toggleModal()" class="flex items-center justify-center w-6 h-6 ml-auto text-indigo-500 rounded hover:bg-indigo-500
+      hover:text-indigo-100" data-modal-toggle="staticModal">
+      <svg class="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
+      </svg>
+    </button>
 
+    <div v-if="showModal"
+      class="overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none justify-center items-center flex">
+      <div class="relative w-auto my-6 mx-auto max-w-6xl">
+        <!--content-->
+        <div
+          class="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+          <!--header-->
+          <div class="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+            <h3 class="text-3xl font-semibold">
+              Enter the details of your card here
+            </h3>
+            <button
+              class="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+              v-on:click="toggleModal()">
+              <span class="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                ×
+              </span>
+            </button>
+          </div>
+          <!--body-->
+          <div class="relative p-6 flex-auto">
+            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title</label>
+            <input type="select" id="password"
+              class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+              required>
+            <label for="cars" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Choose a
+              Status</label>
+            <select id="cars" name="cars">
+              <option value="volvo">New </option>
+              <option value="saab">In Progress</option>
+              <option value="fiat">Completed</option>
+            </select>
+            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Status</label>
+            <input type="select" id="password"
+              class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+              required>
+          </div>
+          <!--footer-->
+          <div class="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+            <button
+              class="text-red-500 bg-transparent border border-solid border-red-500 hover:bg-red-500 hover:text-white active:bg-red-600 font-bold uppercase text-sm px-6 py-3 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+              type="button" v-on:click="toggleModal()">
+              Close
+            </button>
+            <button
+              class="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+              type="button" v-on:click="toggleModal()">
+              Save Changes
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div v-if="showModal" class="opacity-25 fixed inset-0 z-40 bg-black"></div>
+  </form>
 
 </template>
 
 <script>
 import Task from "../store/task"
 export default {
+  name: "large-modal",
   data() {
 
 
     return {
-      mostrarform: true
+      mostrarform: true,
+      showModal: false
     }
+
 
 
 
@@ -59,6 +125,9 @@ export default {
     showform() {
 
       this.mostrarform = !this.mostrarform;
+    },
+    toggleModal: function () {
+      this.showModal = !this.showModal;
     }
 
   }
@@ -70,5 +139,5 @@ export default {
 </script>
 
 <style>
-#defaultModal {}
+
 </style>
