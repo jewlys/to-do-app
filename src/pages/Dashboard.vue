a10ed5e636cb006064ab198617124600e8f129f3
Benjamin Gelb <bengelb7@gmail.com>
2022-12-16 10:40:00 +0100

    commit example

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -24,7 +24,7 @@ Here begins the huge code for the header columns
             class="flex items-center justify-center w-5 h-5 ml-2 text-sm font-semibold text-indigo-500 bg-white rounded bg-opacity-30">{{
                 tasksStore.getTasksbyStatus(1).length
             }}</span>
-          <!-- <button
+          <!--  example comment <button
             class="flex items-center justify-center w-6 h-6 ml-auto text-indigo-500 rounded hover:bg-indigo-500 hover:text-indigo-100">
             <svg class="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6">
