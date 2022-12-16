152f916f63874b84720249c18fa424081f606807
jewlys <jewlys@hotmail.com>
2022-12-16 17:15:38 +0100

    burger fix

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,6 @@
 <template>
 
-  <div class="sticky top-0 flex justify-between items-center  w-screen h-16 px-10 bg-white bg-opacity-75">
+  <div class="sticky top-0 flex items-center flex-shrink-0 w-full h-16 px-10 bg-white bg-opacity-75">
     <img src="../assets/images/potion (1).png" alt="" />
     <p class="mx-2 text-sm font-semibold text-gray-600 hover:text-indigo-700">
       Potion
@@ -8,11 +8,12 @@
 
     <!-- <input class="flex items-center h-10 px-4 ml-10 text-sm bg-gray-200 rounded-full focus:outline-none focus:ring"
             type="" placeholder="Create a new task">  -->
-    <div id="navbarNav" v-show="menuActive" class="flex flex-row  ml-10 ">
-    
-      <burger-menu @toggle-menu="menuActive = !menuActive" :active="menuActive"></burger-menu>
-
-    
+    <div class="flex flex-row  ml-10 ">
+      <div class="burger md:hidden mx-4">
+        <div class="bar1 h-1 w-7 my-1.5 bg-pink-200 duration-300"></div>
+        <div class="bar2 h-1 w-7 my-1.5 bg-pink-200"></div>
+        <div class="bar3 h-1 w-7 my-1.5 bg-pink-200 duration-300"></div>
+      </div>
       <ul class= "links md:flex flex-col md:flex-row md:justify-around items-center my-7 hidden text-center">
         <li>
           <a class="mx-2 text-sm font-semibold text-gray-600 hover:text-indigo-700" href="/landing">Home</a>
@@ -40,22 +41,4 @@
 
 <script>
 
-import Burger from './Burger.vue';
-
-
-export default {
-        name: 'Burger',
-        props: {
-            active: {
-                type: Boolean,
-                required: true,
-                default: false
-            }
-        },
-        methods: {
-            toggleActive () {
-                this.$emit('toggle-menu')
-            }
-        }
-    }
 </script>
